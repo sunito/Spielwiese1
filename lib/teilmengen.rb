5f1d4b3ce1e904d933b70382ed0e17232f9da98a
Sven Suska <sven838git@suska.org>
2015-01-25 15:05:06 +0100

    Erste richtig funktionierende Version, völliges Re-Design

--- lib/teilmengen.rb
+++ lib/teilmengen.rb
@@ -1,24 +1,37 @@
-def alle_teilmengen(array)
-  i = array.size
-  array = array.dup
-  array.sort!
-  alle_teilarrays = [[]]
-  while i > 0 do
-    i -= 1
-    alle_teilarrays << [array[i]]
-    if i > 1
-      z = i
-      teilarray = []
-      while z > 0 do
-        z -= 1
-        teilarray << array[z]
-      end
-      alle_teilarrays << teilarray
+def alle_teilmengen(ausgangsarray)
+  
+  
+  
+  alle_teilarrays = []
+  zahl = 0
+  while zahl.to_s(2).length <= ausgangsarray.length do 
+    teilarray = []
+    # Neues Teilarray finden
+    #Umwdlg. von Dual- in Binärsystem
+    zahlbinaer = zahl.to_s(2)
+    #Hilfsarray mit 0 befüllen
+    hilfsarray = [] 
+    ausgangsarray.length.times do #Hilfsarray mit 0 füllen
+      hilfsarray << 0
     end
+    #In Hilfsarray einsetzen 
+    zahlbinaer.length.times do |i|
+      if zahlbinaer[zahlbinaer.length-1-i].to_i == 1
+        hilfsarray[hilfsarray.length-1-i] = zahlbinaer[zahlbinaer.length-1-i].to_i
+      end
+    end
+    #Teilarray zusammenstellen
+    (ausgangsarray.length).times do |x|
+      if hilfsarray[x] == 1
+        teilarray << ausgangsarray[x]
+      end
+      
+    end
+    alle_teilarrays << teilarray
+    zahl += 1
+
   end
-  alle_teilarrays 
-end 
-
-
+  alle_teilarrays
+end
 
 

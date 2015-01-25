37fb9f978aaf4367b8d7adcaf7ee93f13a51b20a
Sven Suska <sven838git@suska.org>
2015-01-25 16:08:28 +0100

    Code-Vereinfachung

--- lib/teilmengen.rb
+++ lib/teilmengen.rb
@@ -10,20 +10,32 @@ def alle_teilmengen(ausgangsarray)
     #Umwdlg. von Dual- in Binärsystem
     zahlbinaer = zahl.to_s(2)
     #Hilfsarray mit 0 befüllen
+
+=begin
     hilfsarray = [] 
     ausgangsarray.length.times do #Hilfsarray mit 0 füllen
       hilfsarray << 0
     end
+=end
     #In Hilfsarray einsetzen 
-    zahlbinaer.length.times do |i|
-      if zahlbinaer[zahlbinaer.length-1-i].to_i == 1
-        hilfsarray[hilfsarray.length-1-i] = zahlbinaer[zahlbinaer.length-1-i].to_i
-      end
-    end
+    #zahlbinaer.length.times do |i|
+#      if zahlbinaer[zahlbinaer.length-1-i].to_i == 1
+      #  hilfsarray[hilfsarray.length-1-i] = zahlbinaer[zahlbinaer.length-1-i].to_i
+     # end
+ #   end
     #Teilarray zusammenstellen
+    abst = ausgangsarray.length  - zahlbinaer.length
     (ausgangsarray.length).times do |x|
-      if hilfsarray[x] == 1
-        teilarray << ausgangsarray[x]
+      
+      #i = hilfsarray.length - 1 - x
+      #x = hilfsarray.length - 1 - i
+      if x >= abst
+        #hilfsarray[x] = zahlbinaer[x - abst].to_i
+        #hilfsarray[x] = 
+        if zahlbinaer[x - abst].to_i == 1
+          teilarray << ausgangsarray[x]
+          #teilarray = [ausgangsarray[x]] + teilarray
+        end
       end
       
     end
